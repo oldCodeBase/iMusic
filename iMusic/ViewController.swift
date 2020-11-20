4af92afbd59d292963c84c9829e387e358bc268d
Ibraczar <44230748+Ibraczar@users.noreply.github.com>
2020-11-20 12:29:34 +0300

    Initial Commit

---
+++ iMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iMusic
+//
+//  Created by Ibragim Akaev on 20/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
